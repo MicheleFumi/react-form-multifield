0d39d49e451ed28283980a798c43910f11297539
MicheleFumi <michele.fumi96@gmail.com>
2024-11-19 15:40:44 +0100

    add button

--- src/Components/AppMain.jsx
+++ src/Components/AppMain.jsx
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react'
 import titleArray from '../Data/db.jsx'
 
+
 export default function AppMain() {
 
     const [titles, setTitles] = useState(titleArray)
     const [newTitles, setNewTitles] = useState('')
+    const post = {
+        title: '',
+        platforms: '',
+        author: '',
+        published: false
+
+    }
+
+
+
+
+
 
 
     function addTitle(e) {
@@ -51,12 +64,35 @@ export default function AppMain() {
 
 
             <div className="container">
+
+
+
+
+
                 <form onSubmit={addTitle}>
                     <div className="input-group my-5">
                         <input type="text" className="form-control" placeholder="Inserisci un titolo" aria-label="Inserisci un titolo " aria-describedby="button-addon2" value={newTitles} onChange={e => setNewTitles(e.target.value)} />
-                        <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
+                        <input type="text" className="form-control" placeholder="Inserisci le piattaforme" aria-label="Inserisci le piattaforme " aria-describedby="button-addon2" />
                     </div>
+                    <div className="input-group my-5">
+                        <input type="text" className="form-control" placeholder="Inserisci l'autore" aria-label="Inserisci l'autore " aria-describedby="button-addon2" />
+                        <div class="form-check ms-3">
+                            <input class="form-check-input" type="checkbox" value="" id="" />
+                            <label class="form-check-label"> pubblicato </label>
+                        </div>
+
+
+                    </div>
+                    <button className="btn btn-primary btn-lg mb-3 " type="button" popovertarget="off-canvas-form">
+                        <i className="bi bi-plus"></i> Add
+                    </button>
                 </form>
+
+
+
+
+
+
                 <h2 >Titoli Di Oggi</h2>
                 <ul className="list-group mt-4  ">
                     {titles.map((title, index) => <li className="list-group-item d-flex justify-content-between mx-4" key={index}>{title}
