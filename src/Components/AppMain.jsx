d134a8865141db2d3cbbfe69919e666dafbe4e37
MicheleFumi <michele.fumi96@gmail.com>
2024-11-19 16:40:28 +0100

    add code

--- src/Components/AppMain.jsx
+++ src/Components/AppMain.jsx
@@ -1,35 +1,33 @@
 import { useState, useEffect } from 'react'
 import titleArray from '../Data/db.jsx'
+const initialFormData =
+{
+    title: '',
+    platforms: '',
+    author: '',
+    published: false
+}
 
 
 export default function AppMain() {
 
     const [titles, setTitles] = useState(titleArray)
     const [newTitles, setNewTitles] = useState('')
-    const post = {
-        title: '',
-        platforms: '',
-        author: '',
-        published: false
-
-    }
+    const [formData, setFormData] = useState(initialFormData)
 
 
-
-
-
-
-
-    function addTitle(e) {
+    function handleFormSubmit(e) {
         e.preventDefault()
+        console.log('Form sent', formData);
+        const newItem = {
+            ...formData
+        }
+        console.log(e.target.name, e.target.value, e.target.checked);
+        setFormData(initialFormData);
 
-        setTitles([
-            ...titleArray,
-            newTitles
-
-        ])
-        setNewTitles('')
     }
+
+
 
     function handleRemoveTitle(e) {
         const titleToRemove = Number(e.target.getAttribute('data-index'))
@@ -52,6 +50,19 @@ export default function AppMain() {
     }
 
 
+    function handleFormField(e) {
+        //console.log(e.target);
+
+
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+
+        setFormData({
+            ...formData,
+            [e.target.name]: value
+        })
+    }
+
+
 
 
 
@@ -64,26 +75,21 @@ export default function AppMain() {
 
 
             <div className="container">
-
-
-
-
-
-                <form onSubmit={addTitle}>
+                <form onSubmit={handleFormSubmit}>
                     <div className="input-group my-5">
-                        <input type="text" className="form-control" placeholder="Inserisci un titolo" aria-label="Inserisci un titolo " aria-describedby="button-addon2" value={newTitles} onChange={e => setNewTitles(e.target.value)} />
-                        <input type="text" className="form-control" placeholder="Inserisci le piattaforme" aria-label="Inserisci le piattaforme " aria-describedby="button-addon2" />
+                        <input type="text" name='title' className="form-control" placeholder="Inserisci un titolo" aria-label="Inserisci un titolo " aria-describedby="button-addon2" value={formData.title} onChange={handleFormField} />
+                        <input type="text" name='platforms' className="form-control" placeholder="Inserisci le piattaforme" aria-label="Inserisci le piattaforme " aria-describedby="button-addon2" value={formData.platforms} onChange={handleFormField} />
                     </div>
                     <div className="input-group my-5">
-                        <input type="text" className="form-control" placeholder="Inserisci l'autore" aria-label="Inserisci l'autore " aria-describedby="button-addon2" />
-                        <div class="form-check ms-3">
-                            <input class="form-check-input" type="checkbox" value="" id="" />
-                            <label class="form-check-label"> pubblicato </label>
+                        <input type="text" className="form-control" placeholder="Inserisci l'autore" aria-label="Inserisci l'autore " aria-describedby="button-addon2" name='author' value={formData.author} onChange={handleFormField} />
+                        <div className="form-check ms-3">
+                            <input className="form-check-input" type="checkbox" name='published' value={formData.published} onChange={handleFormField} id="" />
+                            <label className="form-check-label"> pubblicato </label>
                         </div>
 
 
                     </div>
-                    <button className="btn btn-primary btn-lg mb-3 " type="button" popovertarget="off-canvas-form">
+                    <button className="btn btn-primary btn-lg mb-3 " type="submit">
                         <i className="bi bi-plus"></i> Add
                     </button>
                 </form>
@@ -95,7 +101,7 @@ export default function AppMain() {
 
                 <h2 >Titoli Di Oggi</h2>
                 <ul className="list-group mt-4  ">
-                    {titles.map((title, index) => <li className="list-group-item d-flex justify-content-between mx-4" key={index}>{title}
+                    {titles.map((title, index) => <li className="list-group-item d-flex justify-content-between mx-4" key={index}>{title.title}
                         <div>
                             <button className='btn btn-warning me-2' onClick={handleChangeTitle} data-index={index}>Cambia</button>
                             <button className='btn btn-danger' onClick={handleRemoveTitle} data-index={index}>Rimuovi</button>
@@ -103,6 +109,7 @@ export default function AppMain() {
 
 
                     </li>)}
+
 
                 </ul>
             </div>
--- src/Data/db.jsx
+++ src/Data/db.jsx
@@ -1,12 +1,29 @@
-export default [
-    "I 10 migliori giochi open-world del 2024",
-    "Come scegliere il miglior setup da gaming",
-    "Le novità di Call of Duty: il capitolo più atteso dell'anno",
-    "Recensione di Cyberpunk 2077: aggiornamenti e miglioramenti",
-    "Guida ai segreti di Elden Ring: esplorazione e boss fight",
-    "Console vs PC: il dibattito infinito sul gaming",
-    "I giochi indie da non perdere quest'anno",
-    "Le migliori offerte su Steam per i saldi autunnali",
-    "Esports: come entrare nel mondo del gaming competitivo",
-    "I classici del retrogaming che non passano mai di moda"
-]
+export default [{
+
+    title: 'I 10 migliori giochi open-world del 2024',
+    platforms: 'Xbox, PlayStation, PC, Nintendo Switch',
+    author: 'michele fumi',
+    published: false
+
+},
+{
+    title: 'I giochi indie da non perdere questo anno',
+    platforms: 'Xbox, PlayStation, PC, Nintendo Switch',
+    author: 'michele fumi',
+    published: true
+
+},
+{
+    title: 'Le migliori offerte su Steam per i saldi autunnali',
+    platforms: 'PC',
+    author: 'michele fumi',
+    published: true
+},
+{
+    title: 'Esports: come entrare nel mondo del gaming competitivo',
+    platforms: 'Xbox, PlayStation, PC, Nintendo Switch',
+    author: 'michele fumi',
+    published: false
+}]
+
+
